d9c9a3d8abed85c2fa3c758f0780e924809461ce
u612 <118947787+u612@users.noreply.github.com>
2026-07-15 15:12:38 +0530

    Add BotConfigRepository to __all__ exports

--- database/repositories/__init__.py
+++ database/repositories/__init__.py
@@ -1,3 +1,4 @@
 from database.repositories.user_repo import UserRepository
+from database.repositories.bot_config_repo import BotConfigRepository
 
-__all__ = ["UserRepository"]
+__all__ = ["UserRepository", "BotConfigRepository"]
